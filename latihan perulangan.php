17242587804134047e002f0731d07a900f2df990
NayaRPL <naya.mmj@gmail.com>
2022-02-17 21:34:02 +0800

    day 68

---
+++ latihan perulangan.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>latihan perulangan php</title>
+</head>
+<body>
+    <?php
+    // perulangan for 
+     for($nilai=0; $nilai < 10 ; $nilai++){
+         echo"<br>";
+         echo" $nilai apa kabar dengan mu"; "<br>";
+    }
+    // perulangan while
+    echo"<br>";
+    $nilai1=3;
+    while($nilai1 < 6){
+        echo " $nilai1 how are you ?";
+        echo"<br>"; 
+        $nilai1++;
+    }
+    ?>
+    
+</body>
+</html>
